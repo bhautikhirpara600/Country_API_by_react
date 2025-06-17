3bb0bc7f97f36518bd28f0e3c1986ac4a4829b3e
Bhautik <bhirpara600@gmail.com>
2025-06-17 15:08:56 +0530

    Change import path in  App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Components/Header"
 import { Outlet } from "react-router"
-import { ThemeProvider } from "./Contexts/themeContext"
+import { ThemeProvider } from "src/Contexts/ThemeContext"
 import { SearchQueryProvider } from "./Contexts/SearchQueryContext"
 
 export default function App() {
